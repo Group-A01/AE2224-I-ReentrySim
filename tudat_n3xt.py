b7f9f6d3701e8bfc564bb6c38d2ea4a1ca42441f
vaskovaselinev <martinbarov@gmail.com>
2025-03-24 08:58:24 +0100

    Years on X axis

--- tudat_n3xt.py
+++ tudat_n3xt.py
@@ -4,6 +4,7 @@ import matplotlib
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
+from datetime import datetime, timedelta  # CHANGED: Added timedelta for date calculations
 
 # Load tudatpy modules
 from tudatpy.interface import spice
@@ -13,15 +14,10 @@ from tudatpy.numerical_simulation import environment_setup, propagation_setup
 from tudatpy.astro import element_conversion
 from tudatpy import constants
 from tudatpy.util import result2array
-
 from tudatpy.astro.time_conversion import DateTime
-from datetime import datetime
 import time
-
-import pymsis #wrapper for the nrlmsis fortran code
-
-import urllib.request #for fetching a txt file with live TLE for n3Xt
-
+import pymsis
+import urllib.request
 from alive_progress import alive_bar
 
 # Load spice kernels
@@ -49,11 +45,11 @@ def density_f(h, lon, lat, time): #long and lat in deg, h in km, time in datetim
     data = pymsis.calculate(timedate, lon, lat, h, geomagnetic_activity=-1, version=2.1)
     return data[0,pymsis.Variable.MASS_DENSITY]
 
-# def const_temp(h, lon, lat, time):
-#     timedate = np.datetime64("2000-01-01T00:00") + np.timedelta64(time, 's')
-#     # print(timedate)
-#     data = pymsis.calculate(timedate, lon, lat, h, geomagnetic_activity=-1, version=2.1)
-#     return data[0,pymsis.Variable.TEMPERATURE]
+def const_temp(h, lon, lat, time):
+    timedate = np.datetime64("2000-01-01T00:00") + np.timedelta64(time, 's')
+    print(timedate)
+    data = pymsis.calculate(timedate, lon, lat, h, geomagnetic_activity=-1, version=2.1)
+    return data[0,pymsis.Variable.TEMPERATURE]
 
 body_settings.get("Earth").atmosphere_settings = environment_setup.atmosphere.custom_four_dimensional_constant_temperature(
     density_f,
@@ -133,6 +129,7 @@ print("2. Specify a custom end date (YYYY-MM-DD)")
 choice = input("Enter your choice (1 or 2): ")
 
 # Set simulation start epoch
+simulation_start_epoch = DateTime(2008, 5, 5).epoch()
 
 # Retrieve the initial state of satellite using Two-Line-Elements (TLEs) (n3xt = 39428U)
 targeturl = "https://celestrak.org/NORAD/elements/gp.php?GROUP=cubesat&FORMAT=tle"
@@ -148,8 +145,6 @@ if inp == "1":
                 tle_data = (lines[i], lines[i+1])
                 break
 elif inp == "2":
-    tle_data = ("1 51074U 22029BR  22249.50000000  .00001103  00000-0  33518-4 0  9995",
-                "2 51074  97.5000   0.7036 0003481  80.0000   0.3331 15.17013610017788")
     tle_data = (
     "1 32789U 08021G   08126.86298982  .00000240  00000-0  21562-4 0   123",
     "2 32789  98.0528 210.5829 0011778 342.5890 017.4188 14.70853390   107"
@@ -256,6 +251,17 @@ states_array = result2array(states)
 dep_vars = dynamics_simulator.propagation_results.dependent_variable_history
 dep_vars_array = result2array(dep_vars)
 
+# Convert time to years and calculate final date
+start_date = datetime(2008, 5, 5)  # Simulation start date
+time_seconds = dep_vars_array[:, 0] - dep_vars_array[0, 0]  # Time in seconds from start
+time_years = time_seconds / (365.25 * 24 * 3600) + 2008  # Convert to years, starting at 2008
+altitude = dep_vars_array[:, 19] / 1000  # Altitude in km
+
+# Calculate final simulation date
+final_time_seconds = time_seconds[-1]
+final_date = start_date + timedelta(seconds=final_time_seconds)
+
+# Plot altitude vs. time in years
 plt.figure(figsize=(9, 5))
 plt.title("Altitude of {0} over time".format(satname))
 plt.plot(time_years, altitude)
